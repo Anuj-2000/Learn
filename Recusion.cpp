20c8bef92ca0c010f62fd9a4bc8c39f787887ff9
Anuj Vishwakarma <anujvishwakarma237@gmail.com>
2025-11-26 23:58:37 +0530

    Fibbonachi using recursion

--- Recusion.cpp
+++ Recusion.cpp
@@ -10,8 +10,17 @@ void printArr(int arr[], int n){
     cout<<arr[n-1]<<" ";
 }
 
+int fibb( int n){
+    //base case
+    if (n==0 || n==1) return n;
+    int res = fibb(n-1) + fibb(n-2);
+    
+
+}
+
 
 int main(){
     int arr[] = {10,20,30,40,50};
     printArr(arr,5); 
+    fibb(5);
 }
