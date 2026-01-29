93cf14629807b726cdcd46d3f304ee3eaa9ee11b
Anuj Vishwakarma <anujvishwakarma237@gmail.com>
2026-01-29 23:28:23 +0530

    pointers

--- pointers.cpp
+++ pointers.cpp
@@ -42,8 +42,8 @@ int main() {
     
     // Exercise 5: Memory leak detection
     // TODO: Intentionally create a leak, then fix it
-    //int* leak = new int[100];
-    // Fix: delete[] leak;
+    int* leak = new int[100];
+    Fix: delete[] leak;
     
     return 0;
 }
